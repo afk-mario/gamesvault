6e659b27e73d1e73048198a250a35d1681324f0a
Mario Carballo Zama <afk@ellugar.co>
2021-08-12 17:24:13 -0500

    feature: Add new game button container

---
+++ containers/game-new-button/index.js
@@ -0,0 +1,92 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import { CgAdd } from 'react-icons/cg';
+import { useForm } from 'react-hook-form';
+import { useRouter } from 'next/router';
+
+import {
+  useCreateGameMutation,
+  useInvalidateAllGamesQuery,
+} from 'hooks/api/games';
+
+import Button from 'components/button';
+import Modal from 'components/modal';
+
+import styles from './style.module.css';
+
+function GameNewButton({ id, onSuccess, onError }) {
+  const router = useRouter();
+  const [showModal, setShowModal] = useState(false);
+  const { register, handleSubmit } = useForm({ defaultValues: { name: '' } });
+  const open = () => setShowModal(true);
+  const close = () => setShowModal(false);
+  const invalidate = useInvalidateAllGamesQuery();
+
+  const mutation = useCreateGameMutation({
+    config: {
+      onSuccess: (data) => {
+        onSuccess(data);
+        invalidate();
+        router.push(`/games/${data[0]}/edit`);
+      },
+      onError: (data) => {
+        onError(data);
+      },
+    },
+  });
+
+  const onSubmit = (values) => {
+    mutation.mutate({
+      title: values.title,
+      _id: '',
+    });
+  };
+
+  return (
+    <>
+      <Button onClick={open}>
+        <CgAdd />
+        <span>New Game</span>
+      </Button>
+      <Modal
+        title="New Game"
+        aria-label="game-new-modal"
+        status="danger"
+        onDismiss={close}
+        isOpen={showModal}
+      >
+        <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+          <label htmlFor="title">Title</label>
+          <input {...register('title')} placeholder="My game" />
+
+          <div className={styles['game-new-button-modal-actions']}>
+            <Button onClick={close}>Cancel</Button>
+            <Button
+              status="danger"
+              loading={mutation.isLoading}
+              disabled={mutation.isLoading}
+              onClick={() => {
+                mutation.mutate(id);
+              }}
+            >
+              <CgAdd />
+              <span>Create</span>
+            </Button>
+          </div>
+        </form>
+      </Modal>
+    </>
+  );
+}
+
+GameNewButton.propTypes = {
+  id: PropTypes.string.isRequired,
+  onSuccess: PropTypes.func,
+  onError: PropTypes.func,
+};
+
+GameNewButton.defaultProps = {
+  onSuccess: () => {},
+  onError: () => {},
+};
+export default GameNewButton;
