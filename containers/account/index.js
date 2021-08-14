e0bd0a821ed8f14c6897566d30b330709b1feddc
Mario Carballo Zama <afk@ellugar.co>
2021-08-14 16:53:37 -0500

    bugfix: Replace old dependency on jazzicon with local version

--- containers/account/index.js
+++ containers/account/index.js
@@ -1,22 +1,20 @@
-import { Jazzicon } from '@ukstv/jazzicon-react';
 import { useEthers, useEtherBalance } from '@usedapp/core';
 import { formatEther } from '@ethersproject/units';
+
+import Jazzicon from 'components/jazzicon';
 
 function Account() {
   const { account } = useEthers();
   const etherBalance = useEtherBalance(account);
   const formattedEther =
     etherBalance && formatEther(etherBalance).substring(0, 6);
+  const seed = parseInt(account.slice(2, 10), 16);
 
-  // const seed = parseInt(account.slice(2, 10), 16);
-  const seed = account;
   return (
     <div className="wallet">
       <div className="balance">{formattedEther} MATIC</div>
       <div className="account">
-        <div style={{ width: '16px', height: '16px' }}>
-          <Jazzicon address={seed} />
-        </div>
+        <Jazzicon diameter={16} seed={seed} />
         <div className="address">{account}</div>
       </div>
     </div>
