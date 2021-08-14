92443a87c5627a6e8a2ad3470c1d645ce8d0d93c
Mario Carballo Zama <afk@ellugar.co>
2021-08-14 16:05:51 -0500

    bugfix: Fix npm dependencies

--- containers/account/index.js
+++ containers/account/index.js
@@ -1,4 +1,4 @@
-import Jazzicon from 'react-jazzicon';
+import { Jazzicon } from '@ukstv/jazzicon-react';
 import { useEthers, useEtherBalance } from '@usedapp/core';
 import { formatEther } from '@ethersproject/units';
 
@@ -8,11 +8,15 @@ function Account() {
   const formattedEther =
     etherBalance && formatEther(etherBalance).substring(0, 6);
 
+  // const seed = parseInt(account.slice(2, 10), 16);
+  const seed = account;
   return (
     <div className="wallet">
       <div className="balance">{formattedEther} MATIC</div>
       <div className="account">
-        <Jazzicon diameter={16} seed={parseInt(account.slice(2, 10), 16)} />
+        <div style={{ width: '16px', height: '16px' }}>
+          <Jazzicon address={seed} />
+        </div>
         <div className="address">{account}</div>
       </div>
     </div>
