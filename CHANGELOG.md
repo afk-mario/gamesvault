fe58c2378f343067919d0012891bc26dc7b89e74
Mario Carballo Zama <afk@ellugar.co>
2021-08-14 17:25:25 -0500

    bugfix: Move developer page to view

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -15,6 +15,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Removed
 
+## [0.5.4] - 2021-08-15
+
+### Fixed
+
+- Move developer to view instead of page
+
 ## [0.5.3] - 2021-08-15
 
 ### Fixed
--- pages/developer/index.js
+++ pages/developer/index.js
@@ -1,56 +1,7 @@
-// import PropTypes from 'prop-types';
-import { Page } from 'components/layouts';
-import { useGetHasValidKeyQuery } from 'hooks/api/unlock';
-import { UNLOCK_DEVELOPER_LOCK_ADDRESS } from 'constants/locks';
-import Spinner from 'components/spinner';
-import DeveloperContentSignedIn from 'containers/developer-content-signed-in';
-import DeveloperSignUp from 'containers/developer-sign-up';
-import Search from 'components/search';
-import DeveloperLinks from 'containers/developer-links';
+import DeveloperView from 'views/developer-view';
 
 function Developer() {
-  const { data, isLoading } = useGetHasValidKeyQuery({
-    lockAddress: UNLOCK_DEVELOPER_LOCK_ADDRESS,
-  });
-
-  return (
-    <Page>
-      <div className="container page-container content-header">
-        <div className="heading-container">
-          <h1 className="page-header">Developer</h1>
-        </div>
-      </div>
-      <div className="container page-container main-container">
-        <section className="sidebar">
-          <Search />
-          <DeveloperLinks />
-        </section>
-        <main className="main">
-          <div className="main-content">
-            <div className="content-box">
-              {isLoading ? (
-                <Spinner />
-              ) : (
-                <>{data ? <DeveloperContentSignedIn /> : <DeveloperSignUp />}</>
-              )}
-            </div>
-          </div>
-        </main>
-
-        <section className="secondary-content">
-          <div className="content-box">
-            <h3>Questions?</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora
-              ea suscipit sequi sapiente ducimus dolorem officia optio?
-              Consequuntur natus rem expedita. Laudantium blanditiis labore quo
-              veniam commodi repudiandae esse officiis!
-            </p>
-          </div>
-        </section>
-      </div>
-    </Page>
-  );
+  return <DeveloperView />;
 }
 
 Developer.propTypes = {};
