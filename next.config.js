fa6b7e4fd0693cb96e4cd23c5c02fae4f8b0a95d
Mario Carballo Zama <afk@ellugar.co>
2021-08-17 10:03:18 -0500

    bugfix: Fleek setup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /* eslint filenames/match-regex: 0 */
 module.exports = {
   reactStrictMode: true,
+  exportTrailingSlash: true,
+  exportPathMap() {
+    return {
+      '/': { page: '/' },
+    };
+  },
 };
